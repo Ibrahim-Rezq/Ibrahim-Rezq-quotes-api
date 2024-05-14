6226b128862d209fe6df114dd57c778a254d37c8
Ibrahim Rezq <ovic391@gmail.com>
2024-05-14 15:12:25 +0300

    updating quote for eslint

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <main className='w-screen h-screen flex bg-[#191919] '>
             <blockquote className='m-auto p-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800 max-w-2xl'>
                 <p className='text-xl italic font-medium leading-relaxed '>
-                    "{quote.content}"
+                    &quot;{quote.content}&quot;
                 </p>
                 <cite>- {quote.author}</cite>
                 <br />
